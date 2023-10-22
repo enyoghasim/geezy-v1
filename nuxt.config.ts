96175949e4a8c9c8179e2d089e41dc5c3aab3a7c
David Enyoghasim <73113145+enyoghasim@users.noreply.github.com>
2023-10-22 23:22:58 +0100

    added google font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: [
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          "Space+Grotesk": {
+            wght: [400, 500, 600, 700],
+          },
+        },
+        display: "swap",
+      },
+    ],
+  ],
+});
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
     <h1>Home page</h1>
+    <h3>Home page</h3>
+    <h1>Home page</h1>
   </div>
 </template>
+<style lang="scss">
+h1 {
+  font-family: "Space Grotesk", sans-serif;
+}
+</style>
